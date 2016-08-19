18ad683f5d3976576120f76353766e8e434493b9
mosane <ayush.singhvi@nyu.edu>
2016-08-19 12:23:21 -0400

    start testing

---
+++ browser/js/dashboard/newDashboard/newDash.spec.js
@@ -0,0 +1,34 @@
+'use strict'
+/* globals module inject chai */
+
+var expect = chai.expect;
+
+describe('New Dashboard', function () {
+
+	beforeEach(module('flashdash'));
+
+	describe('controller `newDashboardCtrl`', function () {
+		let $scope, $state;
+		beforeEach(inject(function($rootScope, $q, $controller, _$state_){
+			$scope = $rootScope.new();
+
+			userFactory = {
+				addDashbaord: chai.spy(function (id, form) {
+					return $q.when('added dashboard to user' + id)
+				})
+			}
+		}))
+
+		describe('.submit scope method', function () {
+			
+			it('uses the `userFactory`', function () {
+				$scope.id = 12;
+				$scope.form = {};
+				$scope.submit();
+				expect($state._mockUrl).not.to.equal('/dashboard')
+				$scope.digest();
+				expect($state._mockUrl).to..equal('/dashboard')
+			})
+		})
+	})
+})

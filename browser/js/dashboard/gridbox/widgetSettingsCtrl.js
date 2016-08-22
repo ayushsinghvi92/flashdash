09f06a93e3d981208f62d3f7797f0d41e6f33cac
Ayush Singhvi <ayush.singhvi@nyu.edu>
2016-08-22 15:49:12 -0400

    Update widgetSettingsCtrl.js

--- browser/js/dashboard/gridbox/widgetSettingsCtrl.js
+++ browser/js/dashboard/gridbox/widgetSettingsCtrl.js
@@ -51,7 +51,6 @@ app.controller('WidgetSettingsCtrl', ['$scope', '$timeout','DashboardFactory', '
           widget.chart.api.refresh();
 
           $interval(function(){
-            console.log('Widgets settings controller');
             $scope.setKeys();
           }, widget.refreshInterval);
         },400)
